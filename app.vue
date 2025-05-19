79df7b9d406f9abef8740d6e908d1cc8fc4647c9
boomNDS <boomsixboom@gmail.com>
2025-05-19 22:16:37 +0700

    init: :tada: projects

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="px-4 py-2 bg-red-300">click</button>
+    <Button>Click me CN</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxt/image']
-})
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
+	modules: [
+		"@nuxt/icon",
+		"@nuxt/fonts",
+		"@nuxt/image",
+		"@vueuse/nuxt",
+		"shadcn-nuxt",
+	],
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: "",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./components/ui",
+	},
+});
